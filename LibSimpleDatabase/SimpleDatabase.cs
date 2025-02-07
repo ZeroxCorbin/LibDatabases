8b97ab47b5e89a50f1224d432cf2692a42b9b40d
Jack <jack@jackb.me>
2025-02-06 21:16:44 -0500

    Improve null safety, error handling, and code readability
    - Removed unnecessary using directives and simplified list initialization.
    - Made various properties and return types nullable for better null safety.
    - Suppressed specific null reference warnings where appropriate.
    - Added finalizers to ensure proper resource cleanup.
    - Updated namespaces and added necessary using directives.
    - Enhanced theme handling and initialization in the application startup process.
    - Improved error handling and logging throughout the codebase.
    - Refactored methods and classes for better readability and resource management.
    - Updated UI elements to reflect changes in data bindings and window types.

--- LibSimpleDatabase/SimpleDatabase.cs
+++ LibSimpleDatabase/SimpleDatabase.cs
@@ -1,156 +1,60 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using SQLite;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
-namespace LibSimpleDatabase
+namespace LibSimpleDatabase;
+
+
+public class SimpleDatabase : ObservableObject
 {
-
-    public class SimpleDatabase : ObservableObject
+    public class SimpleSetting
     {
-        public class SimpleSetting
+        [PrimaryKey]
+        public string? Key { get; set; } = string.Empty;
+        public string? Value { get; set; } = string.Empty;
+    }
+
+    private SQLiteConnection? Connection { get; set; } = null;
+
+    public SimpleDatabase? Open(string dbFilePath)
+    {
+        if (string.IsNullOrEmpty(dbFilePath))
+            return null;
+
+        try
         {
-            [PrimaryKey]
-            public string? Key { get; set; } = string.Empty;
-            public string? Value { get; set; } = string.Empty;
+            Connection ??= new SQLiteConnection(dbFilePath);
+            _ = Connection.CreateTable<SimpleSetting>();
+
+            return this;
         }
-
-        private SQLiteConnection? Connection { get; set; } = null;
-
-        public SimpleDatabase? Open(string dbFilePath)
+        catch (Exception)
         {
-            if (string.IsNullOrEmpty(dbFilePath))
-                return null;
-
-            try
-            {
-                if (Connection == null)
-                    Connection = new SQLiteConnection(dbFilePath);
-
-                Connection.CreateTable<SimpleSetting>();
-
-                return this;
-            }
-            catch (Exception e)
-            {
-                return null;
-            }
+            return null;
         }
+    }
 
-        public T GetValue<T>(string key, bool setDefault = false, TypeNameHandling handling = TypeNameHandling.None, bool noErrors = false)
+    public T? GetValue<T>(string key, bool setDefault = false, TypeNameHandling handling = TypeNameHandling.None, bool noErrors = false)
+    {
+        SimpleSetting? settings = SelectSetting(key);
+        if (settings == null)
         {
-            SimpleSetting settings = SelectSetting(key);
-            if (settings == null)
-            {
-                if (setDefault)
-                    SetValue<T>(key, default(T));
+            if (setDefault)
+                SetValue<T>(key, default);
 
-                return default(T);
+            return default;
+        }
+        else
+        {
+            if (typeof(T) == typeof(string) || typeof(T).IsPrimitive)
+            {
+                return (T)Convert.ChangeType(settings.Value, typeof(T));
             }
             else
             {
-                if (typeof(T) == typeof(string) || typeof(T).IsPrimitive)
-                {
-                    return (T)Convert.ChangeType(settings.Value, typeof(T));
-                }
-                else
-                {
-                    var ser = new JsonSerializerSettings
-                    {
-                        TypeNameHandling = handling
-                    };
-
-                    if (noErrors)
-                        ser.Error = (sender, errorArgs) => errorArgs.ErrorContext.Handled = true;
-
-                    return (T)JsonConvert.DeserializeObject(settings.Value, typeof(T), ser);
-                }
-            }
-        }
-        public T GetValue<T>(string key, T defaultValue, bool setDefault = false, TypeNameHandling handling = TypeNameHandling.None, bool noErrors = false)
-        {
-            SimpleSetting settings = SelectSetting(key);
-            if (settings == null)
-            {
-                if (setDefault)
-                    SetValue<T>(key, defaultValue);
-
-                return defaultValue;
-            }
-            else
-            {
-                if(typeof(T) == typeof(string) || typeof(T).IsPrimitive)
-                {
-                    return (T)Convert.ChangeType(settings.Value, typeof(T));
-                }
-                else
-                {
-                    var ser = new JsonSerializerSettings
-                    {
-                        TypeNameHandling = handling
-                    };
-
-                    if (noErrors)
-                        ser.Error = (sender, errorArgs) => errorArgs.ErrorContext.Handled = true;
-
-                    return (T)JsonConvert.DeserializeObject(settings.Value, typeof(T), ser);
-                }
-            }
-        }
-
-        public List<T> GetAllValues<T>(TypeNameHandling handling = TypeNameHandling.None, bool noErrors = false)
-        {  
-            List<T> lst = new List<T>();
-
-            List<SimpleSetting>? settings = SelectAllSettings();
-            if (settings == null)
-                return lst;
-
-            var ser = new JsonSerializerSettings
-            {
-                TypeNameHandling = handling
-            };
-
-            if (noErrors)
-                ser.Error = (sender, errorArgs) => errorArgs.ErrorContext.Handled = true;
-
-            foreach (SimpleSetting ss in settings)
-            {
-                var res = string.IsNullOrEmpty(ss.Value) ? default : (T)JsonConvert.DeserializeObject(ss.Value, typeof(T), ser); 
-                if (res != null) 
-                    lst.Add(res);
-            }
-            return lst;
-        }
-
-        public void SetValue(string key, string value)
-        {
-            SimpleSetting set = new SimpleSetting()
-            {
-                Key = key,
-                Value = value
-            };
-            _ = InsertOrReplace(set);
-
-            OnPropertyChanged(key);
-        }
-        public void SetValue<T>(string key, T value, TypeNameHandling handling = TypeNameHandling.None, bool noErrors = false)
-        {
-            if(typeof(T) == typeof(string) || typeof(T).IsPrimitive)
-            {
-                SimpleSetting set = new SimpleSetting()
-                {
-                    Key = key,
-                    Value = value?.ToString()
-                };
-                _ = InsertOrReplace(set);
-            }
-            else
-            {
-                var ser = new JsonSerializerSettings
+                JsonSerializerSettings ser = new()
                 {
                     TypeNameHandling = handling
                 };
@@ -158,32 +62,123 @@ namespace LibSimpleDatabase
                 if (noErrors)
                     ser.Error = (sender, errorArgs) => errorArgs.ErrorContext.Handled = true;
 
-                SimpleSetting set = new SimpleSetting()
-                {
-                    Key = key,
-                    Value = JsonConvert.SerializeObject(value, ser)
-                };
-                _ = InsertOrReplace(set);
+                return string.IsNullOrEmpty(settings.Value) ? default : (T?)JsonConvert.DeserializeObject(settings.Value, typeof(T), ser);
             }
-
-            OnPropertyChanged(key);
         }
-
-        public bool ExistsSetting(string key) => Connection?.Table<SimpleSetting>().Where(v => v.Key == key).Count() > 0;
-
-        private int? InsertOrReplace(SimpleSetting setting) => Connection?.InsertOrReplace(setting);
-        public SimpleSetting? SelectSetting(string key) => Connection?.Table<SimpleSetting>().Where(v => v.Key == key).FirstOrDefault();
-        public int? DeleteSetting(string key) { var ret = Connection?.Table<SimpleSetting>().Delete(v => v.Key == key); OnPropertyChanged(key); return ret; }
-        public List<SimpleSetting>? SelectAllSettings() => Connection?.CreateCommand("select * from SimpleSetting").ExecuteQuery<SimpleSetting>();
-
-        public void Close() => Connection?.Dispose();
-        public void Dispose()
+    }
+    public T? GetValue<T>(string key, T? defaultValue, bool setDefault = false, TypeNameHandling handling = TypeNameHandling.None, bool noErrors = false)
+    {
+        SimpleSetting? settings = SelectSetting(key);
+        if (settings == null)
         {
-            Connection?.Close();
-            Connection?.Dispose();
+            if (setDefault)
+                SetValue<T>(key, defaultValue);
 
-            GC.SuppressFinalize(this);
+            return defaultValue;
         }
+        else
+        {
+            if (typeof(T) == typeof(string) || typeof(T).IsPrimitive)
+            {
+                return (T)Convert.ChangeType(settings.Value, typeof(T));
+            }
+            else
+            {
+                JsonSerializerSettings ser = new()
+                {
+                    TypeNameHandling = handling
+                };
+
+                if (noErrors)
+                    ser.Error = (sender, errorArgs) => errorArgs.ErrorContext.Handled = true;
+
+                return string.IsNullOrEmpty(settings.Value) ? default : (T?)JsonConvert.DeserializeObject(settings.Value, typeof(T), ser);
+            }
+        }
+    }
+
+    public List<T> GetAllValues<T>(TypeNameHandling handling = TypeNameHandling.None, bool noErrors = false)
+    {
+        List<T> lst = [];
+
+        List<SimpleSetting>? settings = SelectAllSettings();
+        if (settings == null)
+            return lst;
+
+        JsonSerializerSettings ser = new()
+        {
+            TypeNameHandling = handling
+        };
+
+        if (noErrors)
+            ser.Error = (sender, errorArgs) => errorArgs.ErrorContext.Handled = true;
+
+        foreach (SimpleSetting ss in settings)
+        {
+            T? res = string.IsNullOrEmpty(ss.Value) ? default : (T?)JsonConvert.DeserializeObject(ss.Value, typeof(T), ser);
+            if (res != null)
+                lst.Add(res);
+        }
+        return lst;
+    }
+
+    public void SetValue(string key, string? value)
+    {
+        SimpleSetting set = new()
+        {
+            Key = key,
+            Value = value
+        };
+        _ = InsertOrReplace(set);
+
+        OnPropertyChanged(key);
+    }
+    public void SetValue<T>(string key, T? value, TypeNameHandling handling = TypeNameHandling.None, bool noErrors = false)
+    {
+        if (typeof(T) == typeof(string) || typeof(T).IsPrimitive)
+        {
+            SimpleSetting set = new()
+            {
+                Key = key,
+                Value = value?.ToString()
+            };
+            _ = InsertOrReplace(set);
+        }
+        else
+        {
+            JsonSerializerSettings ser = new()
+            {
+                TypeNameHandling = handling
+            };
+
+            if (noErrors)
+                ser.Error = (sender, errorArgs) => errorArgs.ErrorContext.Handled = true;
+
+            SimpleSetting set = new()
+            {
+                Key = key,
+                Value = JsonConvert.SerializeObject(value, ser)
+            };
+            _ = InsertOrReplace(set);
+        }
+
+        OnPropertyChanged(key);
+    }
+
+    public bool ExistsSetting(string key) => Connection?.Table<SimpleSetting>().Where(v => v.Key == key).Count() > 0;
+
+    private int? InsertOrReplace(SimpleSetting setting) => Connection?.InsertOrReplace(setting);
+    public SimpleSetting? SelectSetting(string key) => Connection?.Table<SimpleSetting>().Where(v => v.Key == key).FirstOrDefault();
+    public int? DeleteSetting(string key) { int? ret = Connection?.Table<SimpleSetting>().Delete(v => v.Key == key); OnPropertyChanged(key); return ret; }
+    public List<SimpleSetting>? SelectAllSettings() => Connection?.CreateCommand("select * from SimpleSetting").ExecuteQuery<SimpleSetting>();
+
+    public void Close() => Connection?.Dispose();
+    public void Dispose()
+    {
+        Connection?.Close();
+        Connection?.Dispose();
+
+        GC.SuppressFinalize(this);
     }
 }
 
